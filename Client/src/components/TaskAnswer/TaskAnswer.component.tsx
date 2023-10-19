21bd37a975395485e97ad4bd73d44d4d61960e36
ArkadiuszWitasik <79448127+ArkadiuszWitasik@users.noreply.github.com>
2023-10-19 23:35:03 +0200

    Split TaskAnswer component into separate components and create dedicated dir with css modules and test

---
+++ Client/src/components/TaskAnswer/TaskAnswer.component.tsx
@@ -0,0 +1,113 @@
+import { FormEvent, useRef } from 'react';
+import { useParams } from 'react-router-dom';
+import { useSendAnswerMutation } from '@/hooks/answer/useSendAnswerMutation';
+import { Flex, Group, ScrollArea, Stack, Text, Textarea, ThemeIcon, Title } from '@mantine/core';
+import { IconClockHour1, IconClockHour11, IconCode } from '@tabler/icons-react';
+import DateTimeDisplay from '../UI/DateTimeDisplay';
+import PrimaryButton from '../UI/PrimaryButton';
+import classes from './TaskAnswer.component.module.css';
+
+interface TaskAnswerFormProps {
+	taskId: number;
+	studentId: number;
+}
+
+interface TaskAnswerHeaderProps {
+	taskNumber: number;
+	lessonNumber: number;
+	taskQuestion: string;
+}
+
+function TaskAnswerHeader({ taskNumber, lessonNumber, taskQuestion }: TaskAnswerHeaderProps) {
+	return (
+		<>
+			<Title fw={700} py={0} order={2}>
+				Zadanie {taskNumber}
+			</Title>
+			<Text fw={500} size='lg' my='xs'>
+				Lekcja {lessonNumber}
+			</Text>
+			<ScrollArea.Autosize type='auto' mah={280} pb='lg' offsetScrollbars='y'>
+				<Text size='md' py='md' px='md' className={classes.taskAnswerQuestionText}>
+					{taskQuestion}
+				</Text>
+			</ScrollArea.Autosize>
+		</>
+	);
+}
+
+function TaskAnswerForm({ taskId, studentId }: TaskAnswerFormProps) {
+	const answerTextareaRef = useRef<HTMLTextAreaElement>(null);
+
+	const sendAnswerMutation = useSendAnswerMutation();
+
+	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		if (!answerTextareaRef.current?.value) {
+			return;
+		}
+		const answer = answerTextareaRef.current?.value;
+		if (answer) {
+			sendAnswerMutation.mutate({
+				solution: answer,
+				taskId: taskId,
+				studentId: studentId,
+			});
+		}
+	};
+	return (
+		<form onSubmit={handleSubmit}>
+			<Stack gap='sm'>
+				<Group gap='lg' align='flex-start'>
+					<ThemeIcon size='lg' variant='transparent'>
+						<IconCode size={36} />
+					</ThemeIcon>
+					<Textarea
+						ref={answerTextareaRef}
+						w='100%'
+						size='md'
+						rows={8}
+						placeholder='Twoja odpowiedź...'
+						className={classes.taskAnswerTextArea}
+					/>
+				</Group>
+				<PrimaryButton type='submit' maw={300} className={classes.taskAnswerPrimaryButton}>
+					Prześlij
+				</PrimaryButton>
+			</Stack>
+		</form>
+	);
+}
+
+function TaskAnswer() {
+	const { id } = useParams();
+
+	const mockData = {
+		taskId: +id!,
+		lessonNumber: 4,
+		taskNumber: 1,
+		studentId: 13,
+		taskQuestion: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. `,
+		openDate: '2023-10-19T19:26:15.000Z',
+		closeDate: '2023-10-26T19:26:15.000Z',
+	};
+
+	return (
+		<Flex px='xl' align='flex-start' justify='space-between'>
+			<Stack w='50%' gap={0}>
+				<TaskAnswerHeader
+					taskNumber={mockData.taskNumber}
+					lessonNumber={mockData.lessonNumber}
+					taskQuestion={mockData.taskQuestion}
+				/>
+				<TaskAnswerForm taskId={mockData.taskId} studentId={mockData.studentId} />
+			</Stack>
+			<Group gap='lg' className={classes.taskAnswerDateDisplayGroup}>
+				<DateTimeDisplay title='Data rozpoczęcia' icon={<IconClockHour1 size={20} />} date={mockData.openDate} />
+				<DateTimeDisplay title='Data zakończenia' icon={<IconClockHour11 size={20} />} date={mockData.closeDate} />
+			</Group>
+		</Flex>
+	);
+}
+
+export default TaskAnswer;
--- Client/src/pages/TaskAnswer.page.tsx
+++ Client/src/pages/TaskAnswer.page.tsx
@@ -1,104 +1,10 @@
-import DateTimeDisplay from '@/components/UI/DateTimeDisplay';
-import PrimaryButton from '@/components/UI/PrimaryButton';
-import { useSendAnswerMutation } from '@/hooks/answer/useSendAnswerMutation';
-import {
-	Flex,
-	Group,
-	ScrollArea,
-	Stack,
-	Text,
-	Textarea,
-	ThemeIcon,
-	Title,
-} from '@mantine/core';
-import { IconClockHour1, IconCode } from '@tabler/icons-react';
-import { IconClockHour11 } from '@tabler/icons-react';
-import { FormEvent, useRef } from 'react';
-import { useParams } from 'react-router-dom';
+import TaskAnswer from '@/components/TaskAnswer/TaskAnswer.component';
 
 function TaskAnswerPage() {
-	const { id } = useParams();
-	const answerTextareaRef = useRef<HTMLTextAreaElement>(null);
-
-	const sendAnswerMutation = useSendAnswerMutation();
-
-	const mockData = {
-		taskId: id,
-		lessonNumber: 4,
-		taskNumber: 1,
-		studentId: 13,
-		taskQuestion: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. `,
-		openDate: '2023-10-19T19:26:15.000Z',
-		closeDate: '2023-10-26T19:26:15.000Z',
-	};
-
-	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		if (!answerTextareaRef.current?.value) {
-			return;
-		}
-		const answer = answerTextareaRef.current?.value;
-		if (answer) {
-			sendAnswerMutation.mutate({
-				solution: answer,
-				taskId: +id!,
-				studentId: mockData.studentId,
-			});
-		}
-	};
-
 	return (
-		<Flex px='xl' align='flex-start' justify='space-between'>
-			<Stack w='50%' gap={0}>
-				<Title fw={700} py={0} order={2}>
-					Zadanie {mockData.taskNumber}
-				</Title>
-				<Text fw={500} size='lg' my='xs'>
-					Lekcja {mockData.lessonNumber}
-				</Text>
-				<ScrollArea.Autosize type='auto' mah={280} pb='lg' offsetScrollbars='y'>
-					<Text size='md' py='md' px='md' style={{ textWrap: 'balance' }}>
-						{mockData.taskQuestion}
-					</Text>
-				</ScrollArea.Autosize>
-				<form onSubmit={handleSubmit}>
-					<Stack gap='sm'>
-						<Group gap='lg' align='flex-start'>
-							<ThemeIcon size='lg' variant='transparent'>
-								<IconCode size={36} />
-							</ThemeIcon>
-							<Textarea
-								ref={answerTextareaRef}
-								w='100%'
-								size='md'
-								rows={8}
-								placeholder='Twoja odpowiedź...'
-								style={{ flex: 1 }}
-							/>
-						</Group>
-						<PrimaryButton
-							type='submit'
-							maw={300}
-							style={{ alignSelf: 'flex-end' }}
-						>
-							Prześlij
-						</PrimaryButton>
-					</Stack>
-				</form>
-			</Stack>
-			<Group gap='lg' style={{ justifySelf: 'flex-end' }}>
-				<DateTimeDisplay
-					title='Data rozpoczęcia'
-					icon={<IconClockHour1 size={20} />}
-					date={mockData.openDate}
-				/>
-				<DateTimeDisplay
-					title='Data zakończenia'
-					icon={<IconClockHour11 size={20} />}
-					date={mockData.closeDate}
-				/>
-			</Group>
-		</Flex>
+		<>
+			<TaskAnswer />
+		</>
 	);
 }
 
