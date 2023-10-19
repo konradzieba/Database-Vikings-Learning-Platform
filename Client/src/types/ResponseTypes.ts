cd8616dc6289b54184e361c6a0c4a88a47020eb0
ArkadiuszWitasik <79448127+ArkadiuszWitasik@users.noreply.github.com>
2023-10-19 23:43:59 +0200

    Expand TMeResponse type to match new data recived from GET /me endpoint

--- Client/src/types/ResponseTypes.ts
+++ Client/src/types/ResponseTypes.ts
@@ -2,8 +2,25 @@ export type TMessageResponse = {
 	message: string;
 };
 
+type TLecturerInfo = {
+	lecturerId: number;
+	isAdmin: boolean;
+	idCheck: number;
+};
+
+type TStudentInfo = {
+	studentId: number;
+	indexNumber: number;
+	score: number;
+	health: number;
+	rank: number;
+	idCheck: number;
+};
+
 export type TMeResponse = {
 	id: number;
 	email: string;
+	firstName: string;
+	lastName: string;
 	role: UserRole;
-};
+} & ({ lecturerInfos: TLecturerInfo } | { studentInfos: TStudentInfo });
