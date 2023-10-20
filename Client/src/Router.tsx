deff7d64b348b1466e6af0121cbdcff2b66907a7
Konrad Zięba <162624@student.uwm.edu.pl>
2023-10-20 16:01:43 +0200

    Create middleware prototype for selecting layout, not working yet

--- Client/src/Router.tsx
+++ Client/src/Router.tsx
@@ -1,5 +1,9 @@
 import { Center } from '@mantine/core';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+	createBrowserRouter,
+	redirect,
+	RouterProvider,
+} from 'react-router-dom';
 import LoginForm from './components/Login/Login.form';
 import BlankContent from './components/UI/BlankContent';
 import StudentLayout from './layouts/Student.layout';
@@ -7,14 +11,12 @@ import MyTasksPage from './pages/MyTasks.page';
 import ScoreBoardPage from './pages/ScoreBoard.page';
 import TaskAnswerPage from './pages/TaskAnswer.page';
 import LecturerLayout from './layouts/Lecturer.layout';
+import { loginMiddleware } from './utils/middlewares';
 
 const router = createBrowserRouter([
 	{
-		path: '/login',
-		element: <LoginForm />,
-	},
-	{
 		path: '/',
+		loader: loginMiddleware,
 		element: <StudentLayout />,
 		children: [
 			{ index: true, element: <Center>STRONA GŁOWNA</Center> },
@@ -25,6 +27,7 @@ const router = createBrowserRouter([
 	},
 	{
 		path: '/dashboard',
+		loader: loginMiddleware,
 		element: <LecturerLayout />,
 		children: [
 			{ index: true, element: <Center>Widok Grup</Center> },
@@ -33,6 +36,10 @@ const router = createBrowserRouter([
 				element: <Center>Podglad jednej grupy</Center>,
 			},
 		],
+	},
+	{
+		path: '/login',
+		element: <LoginForm />,
 	},
 	{ path: '/me', element: <BlankContent /> },
 ]);
---
+++ Client/src/utils/middlewares.ts
@@ -0,0 +1,21 @@
+import { redirect } from 'react-router-dom';
+import axios from './axios';
+import { TMeResponse } from '@/types/ResponseTypes';
+
+export async function loginMiddleware() {
+	try {
+		const meResponse = await axios.get<TMeResponse>('/users/me');
+		if (!meResponse.data) {
+			return redirect('/login');
+		} else {
+			const role = meResponse.data.role;
+			if (role === UserRole.STUDENT) {
+				return redirect('/');
+			} else if (role === UserRole.LECTURER || role === UserRole.SUPERUSER) {
+				return redirect('/dashboard');
+			}
+		}
+	} catch {
+		return redirect('/login');
+	}
+}
