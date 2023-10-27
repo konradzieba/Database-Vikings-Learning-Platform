cfce2066f81a11776b8f1a8e8fb7c72bf260aca4
ArkadiuszWitasik <79448127+ArkadiuszWitasik@users.noreply.github.com>
2023-10-27 16:28:30 +0200

    Improve group hover card styles

--- Client/src/components/Navbar/Lecturer.navbar.tsx
+++ Client/src/components/Navbar/Lecturer.navbar.tsx
@@ -1,20 +1,8 @@
-import {
-	Box,
-	Button,
-	Divider,
-	Flex,
-	Group,
-	HoverCard,
-	Stack,
-	Text,
-	ThemeIcon,
-	rem,
-} from '@mantine/core';
+import { Divider, Flex, Group, HoverCard, rem } from '@mantine/core';
 import { NavLink, useLocation } from 'react-router-dom';
 import classes from './Navbar.module.css';
 import UserPanel from '../UI/UserPanel';
 import { IconChevronDown } from '@tabler/icons-react';
-import { useState } from 'react';
 
 const mockedUserInfo = {
 	email: 'lecturer@test.com',
@@ -25,10 +13,6 @@ const navLinks = [
 		label: 'Grupy',
 		link: '/dashboard',
 	},
-	// {
-	// 	label: 'Ranking',
-	// 	link: '/dashboard/score-board',
-	// },
 ];
 
 const groupNavLinks = [
@@ -50,80 +34,42 @@ function Nav() {
 	const { pathname } = useLocation();
 	return (
 		<Group gap='xl'>
-			{/* {navLinks.map(link => {
-				return (
-					<NavLink
-						to={link.link}
-						className={({ isActive }) =>
-							isActive && pathname.endsWith('dashboard')
-								? `${classes.link} ${classes.activeLink}`
-								: `${classes.link} ${classes.inactiveLink}`
-						}
-						key={link.link}>
-						{link.label}
-					</NavLink>
-				);
-			})} */}
-
 			<Group align='center' gap='sm'>
-				<HoverCard
-					// onOpen={() => setIsMenuOpen(true)}
-					// onClose={() => setIsMenuOpen(false)}
-					offset={10}
-				>
+				<HoverCard offset={10}>
 					<HoverCard.Target>
-						<Group
-							gap={rem(5)}
-							align='center'
-							// size='sm'
-							// variant='transparent'
-							// rightSection={
-							// 	<IconChevronDown size='1.5rem' color='var(--font-color)' />
-							// }
-						>
+						<Group gap={rem(5)} align='center'>
 							<NavLink
 								to={navLinks[0].link}
 								className={({ isActive }) =>
 									isActive && pathname.endsWith('dashboard')
 										? `${classes.link} ${classes.activeLink}`
 										: `${classes.link} ${classes.inactiveLink}`
-								}
-							>
+								}>
 								{navLinks[0].label}{' '}
 							</NavLink>
 							<IconChevronDown size='1.5rem' color='var(--font-color)' />
 						</Group>
 					</HoverCard.Target>
 					<HoverCard.Dropdown px='sm'>
-						<Stack gap='xs'>
-							{['Grupa IV - ISI', 'Grupa II - IO'].map((group) => (
-								<Button variant='default'>
-									<NavLink
-										to='/dashboard/group/1/lessons'
-										className={`${classes.link} ${classes.inactiveLink} ${classes.groupHoverCardItem} ${classes.groupHoverCardItemDropdown}`}
-									>
-										{group}
-									</NavLink>
-								</Button>
-							))}
-						</Stack>
+						{['Grupa IV - ISI', 'Grupa II - IO'].map(group => (
+							<NavLink to='/dashboard/group/1/lessons' className={`${classes.link} ${classes.inactiveLink}`}>
+								{group}
+							</NavLink>
+						))}
 					</HoverCard.Dropdown>
 				</HoverCard>
 			</Group>
 			{pathname.includes('/dashboard/group/') && (
 				<>
 					<Divider orientation='vertical' />
-					{groupNavLinks.map((link) => {
+					{groupNavLinks.map(link => {
 						return (
 							<NavLink
 								to={link.link}
 								className={({ isActive }) =>
-									isActive
-										? `${classes.link} ${classes.activeLink}`
-										: `${classes.link} ${classes.inactiveLink}`
+									isActive ? `${classes.link} ${classes.activeLink}` : `${classes.link} ${classes.inactiveLink}`
 								}
-								key={link.link}
-							>
+								key={link.link}>
 								{link.label}
 							</NavLink>
 						);
