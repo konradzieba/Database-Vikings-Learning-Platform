66ffb5b7c728f2d74d89ad383c42acd7b3fb2d9e
ArkadiuszWitasik <79448127+ArkadiuszWitasik@users.noreply.github.com>
2023-10-28 16:48:08 +0200

    Add optional prop to DateTimeDisplay compoenent to freelyalign title and remove tmp page file

--- Client/src/components/UI/DateTimeDisplay.tsx
+++ Client/src/components/UI/DateTimeDisplay.tsx
@@ -1,17 +1,18 @@
-import { Group, Stack, StackProps, Text, ThemeIcon, rem } from '@mantine/core';
+import { Group, Stack, StackProps, StyleProp, Text, ThemeIcon, rem } from '@mantine/core';
 import dayjs from 'dayjs';
 
 interface DateTimeDisplayProps extends StackProps {
 	date: string;
 	title: string;
 	icon: JSX.Element;
+	titleTextAlgin?: StyleProp<React.CSSProperties['textAlign']>;
 }
 
-function DateTimeDisplay({ date, title, icon, ...stackProps }: DateTimeDisplayProps) {
+function DateTimeDisplay({ date, title, icon, titleTextAlgin = 'end', ...stackProps }: DateTimeDisplayProps) {
 	const formattedDate = dayjs(date).format('D/MM/YYYY, HH:mm');
 	return (
 		<Stack {...stackProps} gap={0}>
-			<Text fw={500} size='md' ta='end'>
+			<Text fw={500} size='md' ta={titleTextAlgin}>
 				{title}
 			</Text>
 			<Group gap={rem(5)}>
