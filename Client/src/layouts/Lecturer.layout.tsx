b762b7f80148d1b65b47dd46c1815338e230c1af
ArkadiuszWitasik <79448127+ArkadiuszWitasik@users.noreply.github.com>
2023-10-31 11:38:09 +0100

    Move all pages to layouts to make GET request easier

--- Client/src/layouts/Lecturer.layout.tsx
+++ Client/src/layouts/Lecturer.layout.tsx
@@ -1,11 +1,22 @@
 import LecturerNavbar from '@/components/Navbar/Lecturer.navbar';
-import { Outlet } from 'react-router-dom';
+import CreateLessonPage from '@/pages/CreateLesson.page';
+import GroupLessonsPage from '@/pages/GroupLessons.page';
+import GroupPanelPage from '@/pages/GroupPanel.page';
+import GroupStudentsInfoPage from '@/pages/GroupStudentsInfo.page';
+import LessonDashboardPage from '@/pages/LessonDashboard.page';
+// import { Outlet } from 'react-router-dom';
 
 function LecturerLayout() {
 	return (
 		<>
 			<LecturerNavbar />
-			<Outlet />
+			{/* <Outlet /> */}
+			{/* All routes from lecturer */}
+			<GroupPanelPage />
+			<GroupLessonsPage />
+			<LessonDashboardPage />
+			<CreateLessonPage />
+			<GroupStudentsInfoPage />
 		</>
 	);
 }
--- Client/src/layouts/Student.layout.tsx
+++ Client/src/layouts/Student.layout.tsx
@@ -1,11 +1,22 @@
 import StudentNavbar from '@/components/Navbar/Student.navbar';
-import { Outlet } from 'react-router-dom';
+import { HomePage } from '@/pages/Home.page';
+import LessonTasksPage from '@/pages/LessonTasks.page';
+import MyTasksPage from '@/pages/MyTasks.page';
+import ScoreBoardPage from '@/pages/ScoreBoard.page';
+import TaskAnswerPage from '@/pages/TaskAnswer.page';
+// import { Outlet } from 'react-router-dom';
 
 function StudentLayout() {
 	return (
 		<>
 			<StudentNavbar />
-			<Outlet />
+			{/* <Outlet /> */}
+			{/* All routes from student */}
+			<HomePage />
+			<ScoreBoardPage />
+			<MyTasksPage />
+			<LessonTasksPage />
+			<TaskAnswerPage />
 		</>
 	);
 }
