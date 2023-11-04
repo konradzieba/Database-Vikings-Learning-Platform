e237293e3cb4cf2dbbaa81fdd2de4e8f66f1b572
ArkadiuszWitasik <79448127+ArkadiuszWitasik@users.noreply.github.com>
2023-11-04 17:12:58 +0100

    Create confirm modal on succes or error when changing password

--- Client/src/hooks/users/useChangeDefaultPasswordMutation.tsx
+++ Client/src/hooks/users/useChangeDefaultPasswordMutation.tsx
@@ -1,4 +1,5 @@
 import { changeDefaultPasswordMutationFn } from '@/utils/axios-queries';
+import { Text } from '@mantine/core';
 import { modals } from '@mantine/modals';
 import { useMutation } from '@tanstack/react-query';
 
@@ -6,11 +7,27 @@ export function useChangeDefaultPasswordMutation() {
 	const changeDefaultPasswordMutation = useMutation({
 		mutationFn: changeDefaultPasswordMutationFn,
 		onSuccess: () => {
-			console.log('success');
 			modals.closeAll();
+			modals.openConfirmModal({
+				title: 'Sukces!',
+				children: <Text size='sm'>Twoje hasło zostało zmienione pomyślnie!</Text>,
+				labels: { confirm: 'Rozumiem', cancel: '' },
+				cancelProps: { display: 'none' },
+				confirmProps: { fullWidth: true },
+			});
 		},
 		onError: error => {
 			console.error(error);
+			modals.closeAll();
+			modals.openConfirmModal({
+				title: 'Błąd...',
+				children: (
+					<Text size='sm'>Podczas zmiany hasła wystąpił nieoczekiwany błąd. Spróbuj po ponownym zalogowaniu.</Text>
+				),
+				labels: { confirm: 'Rozumiem', cancel: '' },
+				cancelProps: { display: 'none' },
+				confirmProps: { fullWidth: true, color: 'var(--bad-state-color)' },
+			});
 		},
 	});
 
