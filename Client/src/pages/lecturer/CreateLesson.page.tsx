afc70834661ff7f7c3e3c984a348ccf6945dce84
ArkadiuszWitasik <79448127+ArkadiuszWitasik@users.noreply.github.com>
2023-11-08 16:30:20 +0100

    Modify steps in create lesson stepper

--- Client/src/pages/lecturer/CreateLesson.page.tsx
+++ Client/src/pages/lecturer/CreateLesson.page.tsx
@@ -1,17 +1,13 @@
 import { Button, Flex, Group, Stack, Stepper, Title, rem } from '@mantine/core';
 import lesson1 from '@/assets/lesson1.png';
 import { useState } from 'react';
-import {
-	IconCheck,
-	IconChecklist,
-	IconList,
-	IconPhoto,
-} from '@tabler/icons-react';
+import { IconCheck, IconChecklist, IconList, IconPhoto } from '@tabler/icons-react';
 import PhotoPicker from '@/components/CreateLesson/PhotoPicker/Photo.picker';
 import FrequencyList from '@/components/FrequencyList/FrequencyList.component';
 import LessonCreated from '@/components/CreateLesson/LessonCreated/LessonCreated.component';
 import test from '@/assets/lessonCreatedImage.png';
 import TasksCardList from '@/components/CreateLesson/TaskCard/TasksCard.list';
+import { useNavigate } from 'react-router-dom';
 
 const data = {
 	lessonNumber: 4,
@@ -38,12 +34,13 @@ const data = {
 
 function CreateLessonPage() {
 	const [activeStep, setActiveStep] = useState(0);
+	const navigate = useNavigate();
 
 	const nextStep = () => {
-		setActiveStep((current) => (current < 3 ? current + 1 : current));
+		setActiveStep(current => (current < 3 ? current + 1 : current));
 	};
 	const prevStep = () => {
-		setActiveStep((current) => (current > 0 ? current - 1 : current));
+		setActiveStep(current => (current > 0 ? current - 1 : current));
 	};
 	const isLastStep = activeStep === 2;
 
@@ -56,27 +53,14 @@ function CreateLessonPage() {
 				mt='xs'
 				active={activeStep}
 				onStepClick={setActiveStep}
-				completedIcon={<IconCheck size='1.2rem' />}
-			>
-				<Stepper.Step
-					allowStepSelect={false}
-					label='Tworzenie zadań'
-					icon={<IconList size='1.2rem' />}
-				>
+				completedIcon={<IconCheck size='1.2rem' />}>
+				<Stepper.Step allowStepSelect={false} label='Tworzenie zadań' icon={<IconList size='1.2rem' />}>
 					<TasksCardList tasksNumbers={data.taskNumbers} />
 				</Stepper.Step>
-				<Stepper.Step
-					allowStepSelect={false}
-					label='Wybór grafiki lekcji'
-					icon={<IconPhoto size='1.2rem' />}
-				>
+				<Stepper.Step allowStepSelect={false} label='Wybór grafiki lekcji' icon={<IconPhoto size='1.2rem' />}>
 					<PhotoPicker avatars={data.avatars} />
 				</Stepper.Step>
-				<Stepper.Step
-					allowStepSelect={false}
-					label='Sprawdzanie obecności'
-					icon={<IconChecklist size='1.2rem' />}
-				>
+				<Stepper.Step allowStepSelect={false} label='Sprawdzanie obecności' icon={<IconChecklist size='1.2rem' />}>
 					<Flex align='center' h={rem(550)}>
 						<FrequencyList w='100%' h='85%' />
 					</Flex>
@@ -86,27 +70,34 @@ function CreateLessonPage() {
 				</Stepper.Completed>
 			</Stepper>
 			<Group mx='auto'>
-				{activeStep < 3 ? (
+				{activeStep === 0 && (
 					<>
-						<Button
-							miw={150}
-							variant='outline'
-							disabled={activeStep == 0}
-							onClick={prevStep}
-						>
+						<Button miw={150} variant='outline' onClick={() => navigate(-1)}>
+							Wróć
+						</Button>
+						<Button miw={150} onClick={nextStep}>
+							Dalej
+						</Button>
+					</>
+				)}
+				{activeStep > 0 && activeStep < 3 && (
+					<>
+						<Button miw={150} variant='outline' onClick={prevStep}>
 							Cofnij
 						</Button>
 						<Button miw={150} onClick={nextStep}>
 							{isLastStep ? 'Stwórz lekcje' : 'Dalej'}
 						</Button>
 					</>
-				) : (
+				)}
+				{activeStep === 3 && (
 					<>
-						{/* Back button only for testing, will be deleted in build */}
-						<Button miw={150} variant='outline' onClick={prevStep}>
-							Cofnij
+						<Button miw={150} onClick={() => console.log('Generowanie pdf')} variant='outline'>
+							Wygeneruj PDF z listą obecności
 						</Button>
-						<Button miw={150}>Wróć do lekcji</Button>
+						<Button miw={150} onClick={() => navigate(`/dashboard/group/9/lessons`)}>
+							Przejdź do wszystkich lekcji
+						</Button>
 					</>
 				)}
 			</Group>
@@ -115,3 +106,28 @@ function CreateLessonPage() {
 }
 
 export default CreateLessonPage;
+
+// {activeStep < 3 ? (
+// 	<>
+
+// 		<Button
+// 			miw={150}
+// 			variant='outline'
+// 			disabled={activeStep == 0}
+// 			onClick={prevStep}
+// 		>
+// 			Cofnij
+// 		</Button>
+// 		<Button miw={150} onClick={nextStep}>
+// 			{isLastStep ? 'Stwórz lekcje' : 'Dalej'}
+// 		</Button>
+// 	</>
+// ) : (
+// 	<>
+// 		{/* Back button only for testing, will be deleted in build */}
+// 		<Button miw={150} variant='outline' onClick={prevStep}>
+// 			Cofnij
+// 		</Button>
+// 		<Button miw={150}>Wróć do lekcji</Button>
+// 	</>
+// )}
