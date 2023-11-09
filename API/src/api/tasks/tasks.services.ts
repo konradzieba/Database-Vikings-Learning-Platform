88ac0cc0dfb2dc7dad1b711e31e10cb7c200df01
Konrad Zięba <162624@student.uwm.edu.pl>
2023-11-09 18:31:26 +0100

    Create getStudentTasks API endpoint

--- API/src/api/tasks/tasks.services.ts
+++ API/src/api/tasks/tasks.services.ts
@@ -1,20 +1,85 @@
-import { Lesson, Prisma, Task } from '@prisma/client';
+import { Group, Lesson, Prisma, Student, Task } from '@prisma/client';
 import { db } from '../../db';
 
 export function getLessonTaskById(id: Task['id']) {
   return db.task.findUnique({
     where: {
-      id
+      id,
     },
-    select : {
+    select: {
       id: true,
       number: true,
       question: true,
       openDate: true,
       closeDate: true,
       lessonId: true,
-    }
-  })
+    },
+  });
+}
+
+export async function getStudentTasks(
+  studentId: Student['id'],
+  groupId: Group['id']
+) {
+  const lessons = await db.lesson.findMany({
+    where: {
+      groupId,
+    },
+    select: {
+      id: true,
+      number: true,
+    },
+  });
+
+  const tasksFromLessons = await db.task.findMany({
+    where: {
+      lessonId: {
+        in: lessons.map((lesson) => lesson.id),
+      },
+    },
+    select: {
+      id: true,
+      number: true,
+      question: true,
+      lessonId: true,
+    },
+  });
+
+  const answers = await db.answer.findMany({
+    where: {
+      studentId,
+      taskId: {
+        in: tasksFromLessons.map((task) => task.id),
+      },
+    },
+  });
+
+  const lessonsData = lessons.map((lesson) => {
+    return {
+      lessonNumber: lesson.number,
+      tasks: tasksFromLessons
+        .filter((task) => task.lessonId === lesson.id)
+        .map((task) => {
+          const correspondingAnswer = answers.find(
+            (answer) => answer.taskId === task.id
+          );
+
+          return {
+            taskNumber: task.number,
+            taskQuestion: task.question,
+            replyStatus: correspondingAnswer?.replyStatus,
+            replyDesc: correspondingAnswer?.replyDesc,
+            solution: correspondingAnswer?.solution,
+            grantedScore: correspondingAnswer?.grantedScore,
+          };
+        })
+        .filter(
+          (task) => task.replyStatus !== null && task.replyStatus !== undefined
+        ),
+    };
+  });
+
+  return lessonsData;
 }
 
 export function findTaskById(id: Task['id']) {
@@ -24,7 +89,6 @@ export function findTaskById(id: Task['id']) {
     },
   });
 }
-
 
 export function createTask(task: Prisma.TaskCreateInput) {
   return db.task.create({
