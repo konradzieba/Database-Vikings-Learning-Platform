ee9f818eede09d07206129b294f72ee42c707c02
ArkadiuszWitasik <79448127+ArkadiuszWitasik@users.noreply.github.com>
2023-11-09 10:06:57 +0100

    Create button in TaskAnswer page to move back to tasks in lesson

--- Client/src/pages/student/TaskAnswer.page.tsx
+++ Client/src/pages/student/TaskAnswer.page.tsx
@@ -1,11 +1,17 @@
 import TaskAnswer from '@/components/TaskAnswer/TaskAnswer.component';
-import { Center } from '@mantine/core';
+import { Button, Flex } from '@mantine/core';
+import { IconArrowBack } from '@tabler/icons-react';
+import { useNavigate } from 'react-router-dom';
 
 function TaskAnswerPage() {
+	const navigate = useNavigate();
 	return (
-		<Center>
+		<Flex direction='column' justify='center'>
+			<Button leftSection={<IconArrowBack />} variant='outline' mx='auto' onClick={() => navigate(-1)}>
+				Powrót do listy zdań
+			</Button>
 			<TaskAnswer />
-		</Center>
+		</Flex>
 	);
 }
 
