eae0576f67c9878416151fa390f429ea65ebaabc
ArkadiuszWitasik <79448127+ArkadiuszWitasik@users.noreply.github.com>
2023-11-10 06:24:32 +0100

    Create function to clear all zustand storages and clear storage after logout mutation

--- Client/src/utils/store.ts
+++ Client/src/utils/store.ts
@@ -12,6 +12,7 @@ interface IUserStoreStore {
 	};
 	setRole: (role: UserRole) => void;
 	setUserData: (userData: IUserStoreStore['userData']) => void;
+	clearUserStoreData: () => void;
 }
 
 interface IStudentStoreStore {
@@ -27,21 +28,22 @@ interface IStudentStoreStore {
 		answersIds: number[] | null;
 	};
 	setStudentData: (studentData: IStudentStoreStore['studentData']) => void;
+	clearStudentStoreData: () => void;
 }
 
-export const useUserStore = create<IUserStoreStore>((set) => ({
-	role: null,
-	userData: {
-		firstName: null,
-		lastName: null,
-		email: null,
-		userId: null,
-	},
-	setRole: (role: UserRole) => set({ role }),
-	setUserData: (userData) => set({ userData }),
-}));
+interface ILecturerStore {
+	lecturerData: {
+		lecturerId: number | null;
+		isAdmin: boolean | null;
+		idCheck: number | null;
+	};
+	groups: TGetGroupsByLecturerId['groups'] | null;
+	setLecturerData: (lecturerData: ILecturerStore['lecturerData']) => void;
+	setGroups: (groups: ILecturerStore['groups']) => void;
+	clearLecturerStoreData: () => void;
+}
 
-export const useStudentStore = create<IStudentStoreStore>((set) => ({
+const initialStudentStoreValues = {
 	studentData: {
 		indexNumber: null,
 		health: null,
@@ -53,26 +55,64 @@ export const useStudentStore = create<IStudentStoreStore>((set) => ({
 		groupId: null,
 		answersIds: null,
 	},
-	setStudentData: (studentData) => set({ studentData }),
-}));
+};
 
-interface ILecturerStore {
-	lecturerData: {
-		lecturerId: number | null;
-		isAdmin: boolean | null;
-		idCheck: number | null;
-	};
-	groups: TGetGroupsByLecturerId['groups'] | null;
-	setLecturerData: (lecturerData: ILecturerStore['lecturerData']) => void;
-	setGroups: (groups: ILecturerStore['groups']) => void;
-}
-export const useLecturerStore = create<ILecturerStore>((set) => ({
+
+const initialLecturerStoreValues = {
 	lecturerData: {
 		lecturerId: null,
 		isAdmin: null,
 		idCheck: null,
 	},
 	groups: null,
-	setLecturerData: (lecturerData) => set({ lecturerData }),
-	setGroups: (groups) => set({ groups }),
+};
+const initialUserStoreValues = {
+	role: null,
+	userData: {
+		firstName: null,
+		lastName: null,
+		email: null,
+		userId: null,
+	},
+};
+
+export const useUserStore = create<IUserStoreStore>(set => ({
+	role: null,
+	userData: {
+		firstName: null,
+		lastName: null,
+		email: null,
+		userId: null,
+	},
+	setRole: (role: UserRole) => set({ role }),
+	setUserData: userData => set({ userData }),
+	clearUserStoreData: () => set(initialUserStoreValues),
+}));
+
+export const useStudentStore = create<IStudentStoreStore>(set => ({
+	studentData: {
+		indexNumber: null,
+		health: null,
+		score: null,
+		rank: null,
+		isPasswordChanged: null,
+		// lastLogin: null,
+		studentId: null,
+		groupId: null,
+		answersIds: null,
+	},
+	setStudentData: studentData => set({ studentData }),
+	clearStudentStoreData: () => set(initialStudentStoreValues),
+}));
+
+export const useLecturerStore = create<ILecturerStore>(set => ({
+	lecturerData: {
+		lecturerId: null,
+		isAdmin: null,
+		idCheck: null,
+	},
+	groups: null,
+	setLecturerData: lecturerData => set({ lecturerData }),
+	setGroups: groups => set({ groups }),
+	clearLecturerStoreData: () => set(initialLecturerStoreValues),
 }));
