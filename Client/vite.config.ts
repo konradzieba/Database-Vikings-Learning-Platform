28ae222dec098c56874d7ce2501105d9a5f9236a
Konrad Zięba <162624@student.uwm.edu.pl>
2023-11-11 18:41:27 +0100

    Prepare vite.config.ts file for docker

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -5,6 +5,11 @@ import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	server: {
+		port: 5173,
+		strictPort: true,
+		host: true,
+	},
 	resolve: {
 		alias: {
 			'@': resolve(__dirname, 'src'),
