892a9560e08001d0df47c974e48e82487de55049
Konrad Zięba <162624@student.uwm.edu.pl>
2023-11-28 16:15:38 +0100

    Change useDeleteLessonMutation directory to correct one

---
+++ Client/src/hooks/groups/useRenameGroupMutation.tsx
@@ -0,0 +1,16 @@
+import { renameGroupMutationFn } from "@/utils/axios-queries";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+export function useRenameGroupMutation(groupId: number, newGroupName: string) {
+	const queryClient = useQueryClient();
+	const mutation = useMutation({
+		mutationFn: () => renameGroupMutationFn(groupId, newGroupName),
+		onSuccess: () => {
+			queryClient.invalidateQueries({
+				queryKey: ['groups'],
+			});
+		},
+	});
+
+	return mutation;
+}
