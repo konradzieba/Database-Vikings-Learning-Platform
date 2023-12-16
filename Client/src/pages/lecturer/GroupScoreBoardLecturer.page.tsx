fec753ce78d8df6792090160faf5fc6717e56d51
Konrad Zięba <162624@student.uwm.edu.pl>
2023-12-16 21:36:43 +0100

    Add group name title above group scoreboard in lecturer layout

--- Client/src/pages/lecturer/GroupScoreBoardLecturer.page.tsx
+++ Client/src/pages/lecturer/GroupScoreBoardLecturer.page.tsx
@@ -3,11 +3,13 @@ import useGetStudentsFromGroup from '@/hooks/groups/useGetStudentsFromGroup';
 import useGetGroupsByLecturerId from '@/hooks/users/useGetGroupsByLecturerId';
 import useGetScoreBoardQuery from '@/hooks/users/useGetScoreBoardQuery';
 import { useLecturerStore } from '@/utils/stores/useLecturerStore';
-import { Center, Loader, Stack, Title } from '@mantine/core';
+import { Center, Loader, Stack, Text, Title } from '@mantine/core';
 import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 function GroupScoreBoardLecturerPage() {
 	const [searchTerm, setSearchTerm] = useState('');
+	const { id } = useParams();
 	const { lecturerData } = useLecturerStore();
 	const { data: groupsData } = useGetGroupsByLecturerId(
 		lecturerData.lecturerId
@@ -30,6 +32,10 @@ function GroupScoreBoardLecturerPage() {
 		);
 	}
 
+	const groupName = groupsData?.groups.find(
+		(group) => group.groupId === +id!
+	)?.groupName;
+
 	const selectLabels = groupsData?.groups?.map((group) => group.groupName);
 
 	const { data: StudentsFromGroup, isPending } = useGetStudentsFromGroup(
@@ -45,7 +51,10 @@ function GroupScoreBoardLecturerPage() {
 	}
 
 	return (
-		<Stack>
+		<Stack maw={1100} mx='auto'>
+			<Title fz='lg' order={1}>
+				Tablica wyników grupy: {groupName}
+			</Title>
 			<ScoreBoardLecturer
 				type='group'
 				scoreBoardData={filteredBySearchTerm}
