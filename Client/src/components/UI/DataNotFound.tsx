2d3a6bbb69f36027955b926b31495570918979e2
Konrad Zięba <162624@student.uwm.edu.pl>
2023-12-16 20:04:49 +0100

    Add optional props for creating student if students array is empty

--- Client/src/components/UI/DataNotFound.tsx
+++ Client/src/components/UI/DataNotFound.tsx
@@ -1,13 +1,32 @@
-import { Center, Flex, Image, Stack, Text, rem } from '@mantine/core';
+import { Button, Center, Flex, Image, Stack, Text, rem } from '@mantine/core';
 import vikingConfused from '@/assets/vikingConfused.webp';
 import classes from './DataNotFound.module.css';
+import { modals } from '@mantine/modals';
 
 interface DataNotFoundProps {
 	firstLineText: string;
 	secondLineText: string;
+	withAddStudentButton?: boolean;
+	groupId?: number;
 }
 
-function DataNotFound({ firstLineText, secondLineText }: DataNotFoundProps) {
+function DataNotFound({
+	firstLineText,
+	secondLineText,
+	withAddStudentButton,
+	groupId,
+}: DataNotFoundProps) {
+	const handleOpenAddStudentToGroupModal = () => {
+		modals.openContextModal({
+			modal: 'addStudentToGroup',
+			title: 'Dodaj studenta do grupy',
+			size: 'md',
+			closeOnClickOutside: false,
+			innerProps: {
+				groupId: +groupId!,
+			},
+		});
+	};
 	return (
 		<Flex justify='center' align='center' h={rem(550)}>
 			<Image src={vikingConfused} alt='happy viking holding beer' />
@@ -22,6 +41,15 @@ function DataNotFound({ firstLineText, secondLineText }: DataNotFoundProps) {
 					<Text fz={rem(50)} fw={500} className={classes.infoText}>
 						{secondLineText}.
 					</Text>
+					{withAddStudentButton && (
+						<Button
+							mx='auto'
+							mt='md'
+							onClick={handleOpenAddStudentToGroupModal}
+						>
+							Dodaj studenta do grupy
+						</Button>
+					)}
 				</Stack>
 			</Center>
 		</Flex>
